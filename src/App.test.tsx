26a30767d83999dfd9be81982a9b6ede8944e5fa
Hassan Abbas <hassan.abbas@worldstores.co.uk>
2021-05-03 17:04:45 +0100

    added ui + tests for multiples

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const checkMultipleof2 = (highlightedNumbers: any) => {
+	let isMultiple = true;
+	highlightedNumbers.forEach((element: any) => {
+		const number: any = element.textContent;
+		if (number % 2 !== 0) {
+			isMultiple = false;
+		}
+	});
+	return isMultiple;
+};
+
+
+
+test('childrens multiplication page should load successfully with nothing selected', () => {
+	render(<App />);
+	const title = screen.getByTestId("title");
+  const container = screen.getByTestId("gridCointainer");
+	const gridElements = screen.getAllByTestId("gridElementNumbers");
+	const unSelectedNumbers = screen.getAllByTestId("unSelectedNumbers");
+	const highlightedNumbers = screen.queryAllByTestId("highlightedNumbers");
+
+  
+  expect(title).toBeInTheDocument();
+  expect(title.textContent).toBe("Children's Multiplication");
+	expect(container).toBeInTheDocument();
+  expect(gridElements.length).toBe(144);
+  expect(unSelectedNumbers.length).toBe(144);
+  expect(highlightedNumbers.length).toBe(0);
+});
+
+test("should highlight the correct multiples of 2 when clicked", () => {
+	render(<App />);
+  fireEvent.click(screen.getByText("2"));
+
+	const unSelectedNumbers = screen.getAllByTestId("unSelectedNumbers");
+	const highlightedNumbers = screen.getAllByTestId("highlightedNumbers");
+
+	expect(checkMultipleof2(highlightedNumbers)).toBeTruthy();
+	expect(unSelectedNumbers.length).toBe(72);
+	expect(highlightedNumbers.length).toBe(72);
+});
+
+
+test("should render correctly when selecting any another number", () => {
+	render(<App />);
+	// clicking 1 should highlight everything
+  fireEvent.click(screen.getByText("1"));
+	let highlightedNumbers = screen.getAllByTestId("highlightedNumbers");
+	let unSelectedNumbers = screen.queryAllByTestId("unSelectedNumbers");
+	
+  expect(highlightedNumbers.length).toBe(144);
+	expect(unSelectedNumbers.length).toBe(0);
+
+  // clicking 2 afterwards should only display multiples of 2 
+  fireEvent.click(screen.getByText("2"));
+	unSelectedNumbers = screen.getAllByTestId("unSelectedNumbers");
+	highlightedNumbers = screen.getAllByTestId("highlightedNumbers");
+
+	expect(checkMultipleof2(highlightedNumbers)).toBeTruthy();
+	expect(unSelectedNumbers.length).toBe(72);
+	expect(highlightedNumbers.length).toBe(72);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  { useState } from "react";
+import styled from "styled-components";
+
+ const Container = styled.div`
+		display: grid;
+		justify-items: space-evenly;
+		grid-gap: 10px;
+		grid-template-columns: repeat(3, 33%);
+		margin: 2rem 5rem;
+
+		@media (max-width: 800px) {
+			grid-template-columns: repeat(2, 50%);
+		}
+
+		@media (max-width: 400px) {
+			grid-template-columns: repeat(1, 100%);
+		}
+ `;
+
+ const GridElement = styled.div`
+		place-self: center;
+ `;
+
+  const Title = styled.h1`
+		display: grid;
+		justify-items: center;
+	`;
+ 
+ const HighlightedNumbers = styled.button`
+		background-color: red;
+		font-weight: bold;
+		font-family: "Arial";
+		font-size: 2rem;
+		line-height: 1.5;
+		cursor: pointer;
+ `;
+
+ const UnselectedNumbers = styled.button`
+		background-color: yellow;
+		font-family: "Arial";
+		font-size: 1.5rem;
+		line-height: 1;
+		cursor: pointer;
+ `;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [selectedNumbers, setNumbers] = useState <number[]>([]);
+  
+  const checkMultiples = (clickedNumber: number) => {
+		let shouldBeHighlighted = [];
+		for (let i = 1; i <= 144; i++) {
+			if (i % clickedNumber === 0) shouldBeHighlighted.push(i);
+		}
+		if (shouldBeHighlighted) setNumbers(shouldBeHighlighted);
+	};
+
+	const displayAllNumbers = () => {
+		let mutiples = [];
+		for (let i: number = 1; i <= 144; i++) {
+     let displayedNumber
+    
+     const stateNumbers: any = Object.values(selectedNumbers);
+      displayedNumber =
+				stateNumbers && stateNumbers.includes(i) ? (
+					<HighlightedNumbers data-testid="highlightedNumbers">
+						{i}
+					</HighlightedNumbers>
+				) : (
+					<UnselectedNumbers data-testid="unSelectedNumbers">
+						{i}
+					</UnselectedNumbers>
+				);
+      	      
+      mutiples.push(
+				<GridElement
+					onClick={() => {
+						checkMultiples(i);
+					}}
+					key={i}
+					data-testid="gridElementNumbers"
+				>
+					{displayedNumber}
+				</GridElement>
+			);
+		}
+		return mutiples;
+	};
+
+	return (
+		<div className="App">
+			<Title data-testid="title">Children's Multiplication</Title>
+			<Container data-testid="gridCointainer">{displayAllNumbers()}</Container>
+		</div>
+	);
 }
 
 export default App;
